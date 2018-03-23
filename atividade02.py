69090b70b451f43d451b8043bac20ee69e63973b
Maycon Willian <mayconwas2018@gmail.com>
2018-03-22 21:40:37 -0300

    Exercicio Lista de Exercicio 2.

---
+++ atividade02.py
@@ -0,0 +1,70 @@
+# 1.Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+#
+# valor = int(input("Digite um valor: "))
+# if valor > 0:
+#     print("O valor é positivo!")
+# else:
+#     print("O valor é negativo!")
+
+# 2.Faça um Programa que peça dois números e imprima o maior deles.
+#
+# n1 = int(input("Digite um número:"))
+# n2 = int(input("Digite o segundo número:"))
+# if n1 >= n2:
+#     print("O primeiro núemro é o maior: ")
+# else:
+#     print("O segundo núemro é o maior: ")
+
+# 3.Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+#
+# valor1 = int(input("Digite um valor para A: "))
+# valor2 = int(input("Digite um valor para B: "))
+# valor3 = int(input("Digite um valor para C: "))
+# soma = valor1 + valor2
+# if soma <= valor3:
+#     print("A soma de A+B é menor que C")
+# else:
+#     print("A soma de A+B é maior que C")
+
+# 4.Faça um Programa que verifique o sexo de uma pessoa. O usuário deve informar ‘F’ ou ‘M’ e o programa deve escrever “Feminino” ou “Masculino”, conforme a letra digitada.
+#
+# sexo = input("Digite f ou m: ")
+# if sexo == "m":
+#     print("Sexo Masculino")
+# elif sexo == "f":
+#     print("Sexo Feminino")
+# else:
+#     print("Sexo indefinido")
+
+# 5.Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar. Dica: utilize o operador módulo (resto da divisão).
+#
+# divisao = int(input("Digite um número inteiro: "))
+# if (divisao % 2 == 0):
+#     print("O número é par!")
+# else:
+#     print("O número é impar!")
+
+# 6.Faça um algoritmo que peça um número e se ele for par some 5, se não, some 8.
+#
+# numero = int(input("Digite um número: "))
+# if (numero % 2 == 0):
+#     print (numero + 5)
+# else:
+#     print (numero + 8)
+
+# 7.Crie um programa que peça uma nota de trabalho e uma de prova (as duas de 0 a 100). Se a média aritmética das notas for maior ou igual a 60, escreva “Aprovado”, se não, “Reprovado”.
+#
+# trabalho = int(input("Digite a nota do trabalho: "))
+# prova = int(input("Digite a nota da prova: "))
+# media = (trabalho+prova)/2
+# if media >= 60:
+#     print("Aprovado!")
+# elif media <= 60:
+#     print("Reprovado!")
+
+# 8.Construa um programa que recebe três valores, A, B e C. Em seguida, apresente na tela somente o maior deles.
+
+a = int(input("Digite um valor para A:"))
+b = int(input("Digite um valor para B:"))
+c = int(input("Digite um valor para C:"))
+if a >= a  and c
