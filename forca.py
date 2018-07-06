5669e883c947a6dd0793d22f7dca3e23fe4ae123
Aluno <mayconwas2018@gmail.com>
2018-07-05 21:32:08 -0300

    continuação trabalho forca

--- forca.py
+++ forca.py
@@ -1,5 +1,6 @@
-def clear():
-    print("\n")
+from random import randint
+
+lista_palavras = ["casa", "mercado", "palio", "palmeiras", "lakers", "lucas"]
 
 print('''==============================
      JOGO DA FORCA - IFPR
@@ -16,10 +17,27 @@ n_aceita = 0
 
 while True:
     inicio = int(input("Digite (1) para Inicar ou (0) para Sair: "))
+
     if inicio == aceita:
+
+        pos= randint (0, len(lista_palavras)-1)
+        palavra = lista_palavras[pos]
+        riscos = [" _ "] * len(palavra)
+
+
         print("\n")
         print("Começando o jogo....FORCA - IFPR")
-        break
+
+        erros = 0
+
+        while erros < 7 :
+            letra = input("Digite uma letra: ")
+
+            #percorrer a palavra e ver se a letra está na palavra
+
+
+        print(palavra)
+        print(riscos)
 
     elif inicio == n_aceita:
         print("Saindo do jogo....")
@@ -27,14 +45,3 @@ while True:
         print("Obrigado!")
         clear()
         break
-
-palavras = [
-    "ifpr",
-    "cachorro",
-    "mundial",
-    "testando",
-    "palavra composta"
-]
-
-letras_digitas = []
-erros = []
