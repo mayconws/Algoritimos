e15478d6a136b43e6a2e6a4716c61f48cceff92a
Aluno <mayconwas2018@gmail.com>
2018-09-13 22:29:55 -0300

    Trabalhando com Banco de Dados Sqlite3

---
+++ aula28.py
@@ -0,0 +1,97 @@
+# Importa uma biblioteca para poder usar seu conteúdo
+import sqlite3
+
+########### F U N Ç Õ E S #########
+
+def criar_tabela_usuario(conexao):
+
+    # Cria o cursor para operar o banco
+    cursor = conexao.cursor()
+
+    # Monta o SQL a ser executado
+    sql = """
+        CREATE TABLE IF NOT EXISTS usuario(
+            nome text,
+            login text,
+            senha text
+        );
+    """
+
+    # Executa um SQL
+    cursor.execute(sql)
+
+# Inserir novo usuário
+def inserir_usuario(conexao):
+
+    # Cria o cursos para operar o banco
+    cursor = conexao.cursor()
+
+    # Pede os dados para o usuário
+    nome = input("Nome: ")
+    login = input("Login: ")
+    senha = input("Senha: ")
+
+    # Monta o select com o format e os dados
+    sql = """
+        INSERT INTO usuario VALUES(
+            '{}',
+            '{}',
+            '{}'
+        );
+    """.format(nome, login, senha)
+
+    # Exemplo:
+    # sql = """
+    #     INSERT INTO usuario VALUES(
+    #         'Maycon Willian',
+    #         'maycon',
+    #         '123'
+    #     );
+    # """
+
+    # Executa o sql
+    cursor.execute(sql)
+
+    # Salvar as modificações.
+    # O commit sempre deve ser feito depois do INSERT, UPDATE ou DELETE
+    conexao.commit()
+
+# Listar os registros de usuário
+def listar_usuarios(conexao):
+
+    # Cria o cursor para operar o banco
+    cursor = conexao.cursor()
+
+    # Monta o SQL
+    sql = "SELECT rowid, * FROM usuario;"
+
+    # Executa o SQL
+    cursor.execute(sql)
+
+    # Armazena os dados (registros) do select
+    # Toda vez que executa o select, precisa usar o fetch para buscar os registros
+    usuarios = cursor.fetchall() # buscar todos
+
+    for u in usuarios:
+        print(u)
+
+def buscar_usuario(conexao):
+
+    cursor = conexao.cursor()
+
+    sql = ""
+
+    # Executa o SQL
+    cursor.execute(sql)
+
+############ P R I N C I P A L #############
+
+# 1º - Iniciar a conexão (ligação) com nosso banco
+print("Conectando no banco...")
+conexao = sqlite3.connect("aula28.sqlite")
+
+listar_usuarios(conexao)
+
+# Fechando a conexão (ligação) com o banco
+print("Fechando conexão com o banco...")
+conexao.close()
